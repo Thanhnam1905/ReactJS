6e9c22d9fdc0383784ff615f63342580904450d9
unknown <namthanhbui93@gmail.com>
2022-01-21 10:57:37 +0700

    edit code

--- src/Components/Cover/index.tsx
+++ src/Components/Cover/index.tsx
@@ -9,26 +9,26 @@ import { Link } from "react-router-dom";
 
 const Cover = () => {
   return (
-    <div className="container">
-      <div className="inner-container">
-        <div className="content">
-          <div className="text-container">
-            <h1 className="content-text">LMS - Learning</h1>
-            <h1 className="content-text">Management System</h1>
-            <Link to="/login">
-              <button className="button">E - Finance</button>
-            </Link>
-          </div>
-          <div className="footer-container">
-            <p className="footer-text">Hệ thống giáo dục được phát triển bởi</p>
+     <div className="container">
+     <div className="inner-container">
+      <div className="content">
+         <div className="text-container">
+          <h1 className="content-text">LMS - Learning</h1>
+          <h1 className="content-text">Management System</h1>
+          <Link to="/login">
+             <button className="button">E - Finance</button>
+          </Link>
+         </div>
+         <div className="footer-container">
+           <p className="footer-text">Hệ thống giáo dục được phát triển bởi</p>
             <img className="footer-logo" src={logoFooter} alt="" />
-          </div>
+         </div>
+     </div>
+      <div className="wrapper-img">
+        <div className="img-container">
+           <img className="img" src={imgLogo} alt="" />
+           <img className="img-circle" src={Circle} alt="" />
         </div>
-        <div className="wrapper-img">
-          <div className="img-container">
-            <img className="img" src={imgLogo} alt="" />
-            <img className="img-circle" src={Circle} alt="" />
-          </div>
           <p className="description">Designed by UI/UX Team of Alta Media</p>
         </div>
       </div>
